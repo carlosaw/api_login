fa31731fe4b204f3feeb6952ca0727e0d860e83d
carlosaw <carlosfreevg@gmail.com>
2024-07-19 10:28:51 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+function Home() {
+    return (<div>
+        <h1>Home</h1>
+
+        <Link href="/sobre">
+            Acessar página Sobre
+        </Link>
+    </div>)
+}
+
+export default Home
